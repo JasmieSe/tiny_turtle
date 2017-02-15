c28b3af1d5f8d259190e7a9309a2383ece48ca0b
Jasmine Serrano <jasmine.se.7@gmail.com>
2017-02-15 14:33:58 +0000

    i changed stuff

---
+++ script.js
@@ -0,0 +1,18 @@
+TinyTurtle.apply(window);
+
+right(30);
+forward(50);
+right(120);
+forward(50);
+right(120);
+forward(50);
+right (269);
+forward(40);
+left(90);
+forward(50);
+left(90);
+forward(40);
+stamp();
+
+
+
